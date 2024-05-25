1e3e8919768d4cde5d778e8103cca5480287e61a
Notkot66 <153978887+Notkot66@users.noreply.github.com>
2024-05-25 19:15:18 +1200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private int speed = 20;
+    public float horizontal;
+    public float vertical;
+    
+
+    
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontal);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * vertical);
+    }
+}
